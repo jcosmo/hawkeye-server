d10dddcbf460eeef0f281979682719377a81892d
James Walker <github@jzwalker.id.au>
2018-02-11 08:31:41 +1100

    Now scanning clubs, grades and teams.

--- src/app.js
+++ src/app.js
@@ -38,29 +38,36 @@ function extractGrades(firstGradesContainer) {
   });
 
   // Process all the rows about the clubs in each grade.
-  firstGradesContainer = firstGradesContainer.next();
-  nextTeamNumber = 1;
+  let firstGradesContainer = firstGradesContainer.next();
+  let nextTeamNumber = 1;
   while (firstGradesContainer && firstGradesContainer.children().first().text().trim() === `${nextTeamNumber}`) {
     // Get all clubs of this number
-    firstGradesContainer.children().filter((i, e) => i % 2 === 1 && i / 2 <= gradesOnRow.length).each((i, e) => {
-      console.log(`Grade ${gradesOnRow[i].fullName}, Team ${nextTeamNumber}: club ${aa(e).text().trim()}`);
-      let teamName = aa(e).text().trim();
-      let clubName;
-      if (match = teamName.match((/^([^\(]+)\(([^\)]+)\)$/))) {
-        clubName = match[1].trim();
-        teamName = match[2].trim();
-      } else {
-        clubName = teamName;
-        teamName = undefined;
-      }
-      club = clubs.find(c => c.name === clubName);
-      if (!club) {
-        club = {id: clubs.length, name: clubName};
-        clubs.push(club);
-      }
-      let team = {id: teams.length, clubId: club.id, gradeId: 6, fixtureNumber: nextTeamNumber, name: teamName};
-      teams.push(team);
-    });
+    firstGradesContainer.children().filter((i, e) => i % 2 === 1 && i / 2 <= gradesOnRow.length).each(
+        (i, e) => {
+          console.log(`Grade ${gradesOnRow[i].fullName}, Team ${nextTeamNumber}: club ${aa(e).text().trim()}`);
+          let teamName = aa(e).text().trim();
+          let clubName;
+          if (match = teamName.match((/^([^\(]+)\(([^\)]+)\)$/))) {
+            clubName = match[1].trim();
+            teamName = match[2].trim();
+          } else {
+            clubName = teamName;
+            teamName = undefined;
+          }
+          let club = clubs.find(c => c.name === clubName);
+          if (!club) {
+            club = {id: clubs.length, name: clubName};
+            clubs.push(club);
+          }
+          let team = {
+            id: teams.length,
+            clubId: club.id,
+            gradeId: gradesOnRow[i].id,
+            fixtureNumber: nextTeamNumber,
+            name: teamName
+          };
+          teams.push(team);
+        });
     nextTeamNumber++;
     firstGradesContainer = firstGradesContainer.next();
   }
@@ -75,43 +82,32 @@ function extractGrades(firstGradesContainer) {
   }
 }
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 app.get('/', function (req, res) {
   res.status(200).send('Hello, world!').end();
 });
 
 app.get('/scrape', function (req, res) {
-  // The URL we will scrape from - in our example Anchorman 2.
-
   reset();
 
   const url = 'http://localhost:3000/nsnta-fixture.html';
 
   console.log(`Loading ${url}`);
-  // The structure of our request call
-  // The first parameter is our URL
-  // The callback function takes 3 parameters, an error, response status code and the html
 
   request(url, function (error, response, html) {
 
-    // First we'll check to make sure no errors occurred when making the request
     if (!error) {
-      // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
 
       aa = cheerio.load(html);
       const firstGradesContainer = aa("div table tr td table tr td table tr td").filter(GRADE_NAME_MATCH).first().parent();
 
       extractGrades(firstGradesContainer);
-      //gradeNodes.each(extractRowOfGrades);
-      /*
-            $('.header').filter(function(){
-              var data = $(this);
-              title = data.children().first().text();
 
-              release = data.children().last().children().text();
-
-              json.release = release;
-            })
-              */
       let content = {clubs: clubs, grades: grades, teams: teams};
       res.status(200).send(content).end();
     }
