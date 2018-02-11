80a8a9d21f7101f22e5bd1dccb5bf38889d37e38
James Walker <github@jzwalker.id.au>
2018-02-12 08:41:49 +1100

    Start extracting the fixture

--- src/app.js
+++ src/app.js
@@ -6,16 +6,21 @@ const app = express();
 const GRADE_NAME_MATCH = function (i, elem) {
   return aa(elem).children().length === 0 && aa(elem).text().match(/^\s+[A-D] GRADE|RESERVE|SPECIAL( [1-9])?\s+$/);
 };
+const ROUND_NAME_MATCH = function (i, elem) {
+  return aa(elem).children().length === 0 && aa(elem).text().match(/^\s+Round 1\s+$/);
+};
 
 let clubs = [];
 let grades = [];
 let teams = [];
+let fixture = {};
 let aa;
 
 function reset() {
   grades = [];
   clubs = [];
   teams = [];
+  fixture = {};
 }
 
 function extractGrades(firstGradesContainer) {
@@ -38,13 +43,12 @@ function extractGrades(firstGradesContainer) {
   });
 
   // Process all the rows about the clubs in each grade.
-  let firstGradesContainer = firstGradesContainer.next();
+  firstGradesContainer = firstGradesContainer.next();
   let nextTeamNumber = 1;
   while (firstGradesContainer && firstGradesContainer.children().first().text().trim() === `${nextTeamNumber}`) {
     // Get all clubs of this number
     firstGradesContainer.children().filter((i, e) => i % 2 === 1 && i / 2 <= gradesOnRow.length).each(
         (i, e) => {
-          console.log(`Grade ${gradesOnRow[i].fullName}, Team ${nextTeamNumber}: club ${aa(e).text().trim()}`);
           let teamName = aa(e).text().trim();
           let clubName;
           if (match = teamName.match((/^([^\(]+)\(([^\)]+)\)$/))) {
@@ -82,11 +86,48 @@ function extractGrades(firstGradesContainer) {
   }
 }
 
+function extractFixture(row) {
+  /*
+    load() {
+    let json = {
+      rounds: [
+        {number: 1, date: '08 Feb 2018', schedule:{1:8,3:6,5:4,7:2}},
+        {number: 2, date: '15 Feb 2018', schedule:{2:5,4:3,6:1,8:7}},
+        {number: 3, date: '22 Feb 2018', schedule:{1:4,3:2,5:7,8:6}},
+        {number: 4, date: '01 Mar 2018', schedule:{1:4,3:2,5:7,8:6}},
+      ]
+    };
+   */
+  let month;
+  let done = false;
+  let rounds = [];
+  while (!done) {
+    let columns = row.children();
+    let match = columns.first().text().match(/\s+Round (\d+)\s+/);
+    if (match) {
+      let round = match[1];
+      console.log(`Got round ${match[1]}`);
+      if ( columns.eq(1).text().trim().length > 0 ) {
+        month = columns.eq(1).text().trim();
+      }
+      let day = columns.eq(2).text().trim();
+      let date = `${day} ${month}`;
+      rounds.push({number: round, date: date, schedule: {}})
+    }
+    row = row.next();
+    if (row.children().length <= 0) {
+      done = true;
+    }
+  }
+  fixture.rounds = rounds;
+}
+
+
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-})
+});
 
 app.get('/', function (req, res) {
   res.status(200).send('Hello, world!').end();
@@ -105,10 +146,18 @@ app.get('/scrape', function (req, res) {
 
       aa = cheerio.load(html);
       const firstGradesContainer = aa("div table tr td table tr td table tr td").filter(GRADE_NAME_MATCH).first().parent();
-
       extractGrades(firstGradesContainer);
 
-      let content = {clubs: clubs, grades: grades, teams: teams};
+      const firstRoundContainer = aa("div table tr td table tr td table tr td").filter(ROUND_NAME_MATCH).first().parent();
+      extractFixture(firstRoundContainer);
+
+      console.log(`${clubs.length} clubs, ${grades.length} grades, ${teams.length} teams`);
+      let content = {
+        fixture: fixture,
+        clubs: clubs,
+        grades: grades,
+        teams: teams
+      };
       res.status(200).send(content).end();
     }
     else {
