9a68dd773419e464fcab5c3b9bd873a2d59a766c
James Walker <github@jzwalker.id.au>
2018-02-23 09:02:34 +1100

    Continue scraping ladder information

--- src/app.js
+++ src/app.js
@@ -4,16 +4,17 @@ const cheerio = require('cheerio');
 const app = express();
 
 const GRADE_NAME_MATCH = function (i, elem) {
-  return aa(elem).children().length === 0 && aa(elem).text().match(/^\s+[A-D] GRADE|RESERVE|SPECIAL( [1-9])?\s+$/);
+  return aa(elem).children().length === 0 && aa(elem).text().match(/^\s*[A-D] GRADE|RESERVE|SPECIAL( [1-9])?\s*$/);
 };
 const ROUND_NAME_MATCH = function (i, elem) {
-  return aa(elem).children().length === 0 && aa(elem).text().match(/^\s+Round 1\s+$/);
+  return aa(elem).children().length === 0 && aa(elem).text().match(/^\s*Round 1\s*$/);
 };
 
 let clubs = [];
 let grades = [];
 let teams = [];
 let fixture = {};
+let ladders = [];
 let aa;
 
 function reset() {
@@ -21,6 +22,7 @@ function reset() {
   clubs = [];
   teams = [];
   fixture = {};
+  ladders = [];
 }
 
 function extractGrades(firstGradesContainer) {
@@ -86,6 +88,89 @@ function extractGrades(firstGradesContainer) {
   }
 }
 
+
+/*
+{
+  round:
+  gradeId:
+  teams: [
+    {
+      teamId:
+      points:
+      percentage:
+    }
+  ]
+ */
+function extractLadders(firstLadderContainer) {
+  // Get the grade names from the container, then get all the clubs from siblings until we reach a blank row.
+  // After a blank row keep looking until we find another block of grades, then repeat.
+  const laddersOnRow = [];
+
+  aa(firstLadderContainer).children().filter(GRADE_NAME_MATCH).each(function (i, elem) {
+    const gradeName = aa(this).text().trim();
+    const ladder = {id: ladders.length, gradeId: -1, round: -1, teams: [] };
+    ladder.gradeId = grades.find(g => (g.fullName === gradeName )).id;
+    laddersOnRow.push(ladder);
+    ladders.push(ladder)
+  });
+
+
+  // Process all the rows about the ladder in each grade.
+  [1,2,3,4,5,6,7,8].forEach( (i) => {
+    firstLadderContainer = firstLadderContainer.next();
+    let index = 0;
+    laddersOnRow.forEach( ladder => {
+      const team = {teamId: -1, points: 0, percentage: 0};
+      team.name = firstLadderContainer.children().eq(index * 6 + 1).text().trim();
+      ladder.teams.push(team);
+      index = index + 1;
+    })
+  } );
+
+  return;
+
+  let nextTeamNumber = 1;
+  while (firstLadderContainer && firstLadderContainer.children().first().next().text().trim().length > 0) {
+    // Get all clubs of this number
+    firstLadderContainer.children().filter((i, e) => i % 2 === 1 && i / 2 <= laddersOnRow.length).each(
+        (i, e) => {
+          let teamName = aa(e).text().trim();
+          let clubName;
+          if (match = teamName.match((/^([^\(]+)\(([^\)]+)\)$/))) {
+            clubName = match[1].trim();
+            teamName = match[2].trim();
+          } else {
+            clubName = teamName;
+            teamName = undefined;
+          }
+          let club = clubs.find(c => c.name === clubName);
+          if (!club) {
+            club = {id: clubs.length, name: clubName};
+            clubs.push(club);
+          }
+          let team = {
+            id: teams.length,
+            clubId: club.id,
+            gradeId: laddersOnRow[i].id,
+            fixtureNumber: nextTeamNumber,
+            name: teamName
+          };
+          teams.push(team);
+        });
+    nextTeamNumber++;
+    firstLadderContainer = firstLadderContainer.next();
+  }
+
+  // Skip to the next set of grades
+  while (firstLadderContainer.children().length > 0 && firstLadderContainer.children().filter(GRADE_NAME_MATCH).length === 0) {
+    firstLadderContainer = firstLadderContainer.next();
+  }
+
+  if (firstLadderContainer.children().filter(GRADE_NAME_MATCH).length > 0) {
+    extractGrades(firstLadderContainer);
+  }
+}
+
 function extractFixture(row) {
   /*
     load() {
@@ -116,7 +201,7 @@ function extractFixture(row) {
       [0, 1, 2, 3].forEach(i => {
         const home = parseInt(columns.eq(3 + (i * 3)).text().trim(), 10);
         const away = parseInt(columns.eq(3 + (i * 3) + 2).text().trim(), 10);
-        if ( !home ) {
+        if (!home) {
           bad = true;
         }
         schedule[home] = away;
@@ -133,6 +218,55 @@ function extractFixture(row) {
   fixture.rounds = rounds;
 }
 
+function sendResult() {
+  let content = {
+    fixture: fixture,
+    clubs: clubs,
+    grades: grades,
+    teams: teams
+  };
+
+  res.status(200).send(content).end();
+}
+
+function scrape(res, req) {
+  reset();
+
+  const url = 'http://localhost:3000/nsnta-fixture.html';
+  console.log(`Loading fixture from ${url}`);
+  request(url, function (error, response, html) {
+    if (!error) {
+      aa = cheerio.load(html);
+      const firstGradesContainer = aa("div table tr td table tr td table tr td").filter(GRADE_NAME_MATCH).first().parent();
+      extractGrades(firstGradesContainer);
+      const firstRoundContainer = aa("div table tr td table tr td table tr td").filter(ROUND_NAME_MATCH).first().parent();
+      extractFixture(firstRoundContainer);
+      console.log(`Fixture: ${clubs.length} clubs, ${grades.length} grades, ${teams.length} teams`);
+
+      request('http://localhost:3000/nsnta-ladder.html', (error, response, html) => {
+        if (!error) {
+          aa = cheerio.load(html);
+          const firstLaddersContainer = aa("div table tr td table tr td table tr td").filter(GRADE_NAME_MATCH).first().parent();
+          extractLadders(firstLaddersContainer);
+        }
+
+        let content = {
+          ladders: ladders,
+          //fixture: fixture,
+          //clubs: clubs,
+          //grades: grades,
+          //teams: teams
+        };
+        if (res) {
+          res.status(200).send(content).end();
+        }
+        else {
+          console.log(JSON.stringify(content));
+        }
+      });
+    }
+  });
+}
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -145,38 +279,13 @@ app.get('/', function (req, res) {
 });
 
 app.get('/scrape', function (req, res) {
-  reset();
-
-  const url = 'http://localhost:3000/nsnta-fixture.html';
-
-  console.log(`Loading ${url}`);
-
-  request(url, function (error, response, html) {
-
-    if (!error) {
-
-      aa = cheerio.load(html);
-      const firstGradesContainer = aa("div table tr td table tr td table tr td").filter(GRADE_NAME_MATCH).first().parent();
-      extractGrades(firstGradesContainer);
-
-      const firstRoundContainer = aa("div table tr td table tr td table tr td").filter(ROUND_NAME_MATCH).first().parent();
-      extractFixture(firstRoundContainer);
-
-      console.log(`${clubs.length} clubs, ${grades.length} grades, ${teams.length} teams`);
-      let content = {
-        fixture: fixture,
-        clubs: clubs,
-        grades: grades,
-        teams: teams
-      };
-      res.status(200).send(content).end();
-    }
-    else {
-      res.status(200).send('foo').end();
-    }
-  });
+  scrape(req, res);
 });
 
-app.listen('8088');
-console.log('Magic happens on port 8088');
-exports = module.exports = app;
+if (false) {
+  app.listen('8088');
+  console.log('Magic happens on port 8088');
+  exports = module.exports = app;
+} else {
+  scrape(undefined, undefined);
+}
